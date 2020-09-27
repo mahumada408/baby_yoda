7dc732e1c9f13a6772cc7b3330479d23b8ffb846
Manuel Ahumada <mahumada90@gmail.com>
2020-09-27 16:16:02 -0700

    Working plot

--- scripts/get_openpose_coordinates.py
+++ scripts/get_openpose_coordinates.py
@@ -3,7 +3,29 @@ import os
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.cm as cm
 # import openpose as op
+
+def animate_scatters(iteration, data, scatter):
+    """
+    Update the data held by the scatter plot and therefore animates it.
+    Args:
+        iteration (int): Current iteration of the animation
+        data (list): List of the data positions at each iteration.
+        scatters (list): List of all the scatters (One per element)
+    Returns:
+        list: List of scatters (One per element) with new coordinates
+    """
+    points = np.zeros((1,2))
+    for part in data:
+      points = np.append(points, np.zeros((1,2)), axis=0)
+      points[-1, 0] = data[part][iteration, 0]
+      points[-1, 1] = data[part][iteration, 1]
+    points = np.delete(points, 0, 0)
+    scatter.set_offsets(points)
+    colors = cm.rainbow(np.linspace(0, 1, 25))
+    scatter.set_color(colors)
 
 def main():
   parser = argparse.ArgumentParser()
@@ -66,6 +88,11 @@ def main():
                    "RHeel": np.zeros((1,3)),
                    "Background": np.zeros((1,3))}
 
+  c, s = np.cos(np.pi), np.sin(np.pi)
+  j = np.matrix([[c, s], [-s, c]])
+
+  x_point = 0
+  y_point = 0
   for json_file in listdir:
     loaded_json = json.load(open(args.json_dir+'/'+json_file))
     all_points = loaded_json['people'][0]['pose_keypoints_2d']
@@ -75,14 +102,38 @@ def main():
       if (i % 3) == 0:
         # Make a new row
         body_dictionary[body_parts[body_index]] = np.append(body_dictionary[body_parts[body_index]], np.zeros([1,3]), axis=0)
-      body_dictionary[body_parts[body_index]][-1,i % 3] = all_points[i]
+      if (i % 3) == 0:
+        # Rotate x
+        x_point = all_points[i]
+      elif (i % 3) == 1:
+        # Rotate y
+        y_point = all_points[i]
+      else:
+        percent = all_points[i]
+        point = np.dot(j, [x_point, y_point]).T
+        body_dictionary[body_parts[body_index]][-1,0:3] = np.array([point[0], point[1], percent])
 
   # Delete the zero elements
   for element in body_parts:
     body_dictionary[element] = np.delete(body_dictionary[element], 0, 0)  
 
-  # plt.plot(body_dictionary["Nose"][0], body_dictionary["Nose"][1])
-  # plt.show()
+  print(body_dictionary["Nose"])
+
+  # Attaching axis to the figure
+  fig = plt.figure()
+  ax = plt.axes(xlim=(-960, 0), ylim=(-720,0))
+
+  # Initialize scatter
+  scatter = ax.scatter(body_dictionary["Nose"][0,0], body_dictionary["Nose"][0,1])
+
+  # Number of iterations
+  iterations = len(body_dictionary["Nose"])
+
+  ani = FuncAnimation(fig, animate_scatters, iterations, fargs=(body_dictionary, scatter),
+                                      interval=40, blit=False, repeat=True)
+
+  # anim.save('test.gif', writer='imagemagick')
+  plt.show()
 
 if __name__ == '__main__':
     main()
